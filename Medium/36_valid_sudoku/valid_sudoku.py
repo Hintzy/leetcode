653f6c754c6d495daddd9185e87362013d7537e8
David Hintz <dhintz@gmail.com>
2023-05-13 14:13:57 -0400

    Modified test_series function and imported to valid_sudoku solution

--- Medium/36_valid_sudoku/valid_sudoku.py
+++ Medium/36_valid_sudoku/valid_sudoku.py
@@ -1,7 +1,8 @@
+from ds_templates import test_series
+from test_cases import tests
 
 
 def valid_sudoku(board: list[list[str]]) -> bool:
-
     def valid_check(hm: dict, char):
         if not char.isdigit():
             return True
@@ -37,13 +38,5 @@ def valid_sudoku(board: list[list[str]]) -> bool:
 
     return True
 
-board = [["5","3",".",".","7",".",".",".","."],
-         ["6",".",".","1","9","5",".",".","."],
-         [".","9","8",".",".",".",".","6","."],
-         ["8",".",".",".","6",".",".",".","3"],
-         ["4",".",".","8",".","3",".",".","1"],
-         ["7",".",".",".","2",".",".",".","6"],
-         [".","6",".",".",".",".","2","8","."],
-         [".",".",".","4","1","9",".",".","5"],
-         [".",".",".",".","8",".",".","7","9"]]
 
+test_series.test_series(valid_sudoku, tests)
--- ds_templates/test_series.py
+++ ds_templates/test_series.py
@@ -1,21 +1,20 @@
-if __name__ == '__main__':
+def test_series(func, test_list):
+    for i, case in enumerate(test_list):
+        try:
+            if func(**case['input']) == case['output']:
+                print(f'Test Case {i + 1}: PASS')
+            else:
+                print(f'Test Case {i + 1}: FAIL')
+            print(f'\nInput: {case["input"]}')
+            print(f'Expected Output: {case["output"]}')
+            print(f'Actual Output: {func(**case["input"])}\n')
 
-    def test_series(func, test_list):
-        for i, case in enumerate(test_list):
-            try:
-                if func(**case['input']) == case['output']:
-                    print(f'Test Case {i + 1}: PASS')
-                else:
-                    print(f'Test Case {i + 1}: FAIL')
-                print(f'Input: {case["input"]}')
-                print(f'Expected Output: {case["output"]}')
-                print(f'Actual Output: {func(**case["input"])}')
 
-            except IndexError:
-                print(f'Index error.')
+        except IndexError:
+            print(f'Index error.')
 
-            except KeyError:
-                print(f'Key does not exist.')
+        except KeyError:
+            print(f'Key does not exist.')
 
-            finally:
-                pass
+        finally:
+            pass
